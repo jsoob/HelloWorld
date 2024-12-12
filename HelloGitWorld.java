8ae247230a0ad78ba8ceeb450ec742907954789b
js <kmhe0128@naver.com>
2024-12-12 14:38:02 +0900

    Update HelloGitWorld.java
    부가적인 설명 없음

--- HelloGitWorld.java
+++ HelloGitWorld.java
@@ -2,5 +2,6 @@ public class HelloGitWorld {
 	public static void main(String[] args) {
 		System.out.println("Git Test");
 		System.out.println("Git Test2222222");
+		System.out.println("Git Test3333333");
 	}
 }
